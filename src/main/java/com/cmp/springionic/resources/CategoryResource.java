343303706089c5f6a311ba09f5385dbdca2a5511
Cleriston Pereira <cleriston.melo.pereira@gmail.com>
2024-05-14 15:48:45 -0300

    Create repository and service for Category

--- src/main/java/com/cmp/springionic/resources/CategoryResource.java
+++ src/main/java/com/cmp/springionic/resources/CategoryResource.java
@@ -1,27 +1,25 @@
 package com.cmp.springionic.resources;
 
-import java.util.ArrayList;
-import java.util.List;
-
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.cmp.springionic.domain.Category;
+import com.cmp.springionic.services.CategoryService;
 
 @RestController
 @RequestMapping(value="/categories")
 public class CategoryResource {
+	
+	@Autowired
+	private CategoryService service;
 
-	@GetMapping
-	public List<Category> listar() {
-		Category cat1 = new Category(1L, "Informática");
-		Category cat2 = new Category(1L, "Escritório");
-		
-		List<Category> list = new ArrayList<>();
-		list.add(cat1);
-		list.add(cat2);
-		
-		return list;
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<Category> find(@PathVariable Long id) {
+		Category obj = service.search(id);
+		return ResponseEntity.ok().body(obj);
 	}
 }
---
+++ src/main/java/com/cmp/springionic/services/CategoryService.java
@@ -0,0 +1,21 @@
+package com.cmp.springionic.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.cmp.springionic.domain.Category;
+import com.cmp.springionic.repositories.CategoryRepository;
+
+@Service
+public class CategoryService {
+	
+	@Autowired
+	private CategoryRepository repository;
+
+	public Category search(Long id) {
+		Optional<Category> obj = repository.findById(id);
+		return obj.orElse(null);
+	}
+}
