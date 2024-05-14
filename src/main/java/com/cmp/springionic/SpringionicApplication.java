1f3704830adc712c4524e19a71b84139457760cd
Cleriston Pereira <cleriston.melo.pereira@gmail.com>
2024-05-14 16:24:56 -0300

    Product and many to many association

--- src/main/java/com/cmp/springionic/SpringionicApplication.java
+++ src/main/java/com/cmp/springionic/SpringionicApplication.java
@@ -8,13 +8,18 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.cmp.springionic.domain.Category;
+import com.cmp.springionic.domain.Product;
 import com.cmp.springionic.repositories.CategoryRepository;
+import com.cmp.springionic.repositories.ProductRepository;
 
 @SpringBootApplication
 public class SpringionicApplication implements CommandLineRunner {
 	
 	@Autowired
 	private CategoryRepository categoryRepository;
+	
+	@Autowired
+	private ProductRepository productRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringionicApplication.class, args);
@@ -25,8 +30,18 @@ public class SpringionicApplication implements CommandLineRunner {
 		Category cat1 = new Category(null, "Informática");
 		Category cat2 = new Category(null, "Escritório");
 		
-		categoryRepository.saveAll(Arrays.asList(cat1, cat2));
+		Product p1 = new Product(null, "Computador", 2000.00);
+		Product p2 = new Product(null, "Impressora", 800.00);
+		Product p3 = new Product(null, "Mouse", 80.00);
 		
+		cat1.getProducts().addAll(Arrays.asList(p1, p2, p3));
+		cat2.getProducts().addAll(Arrays.asList(p2));
+		
+		p1.getCategories().addAll(Arrays.asList(cat1));
+		p2.getCategories().addAll(Arrays.asList(cat1, cat2));
+		p3.getCategories().addAll(Arrays.asList(cat1));
+		
+		categoryRepository.saveAll(Arrays.asList(cat1, cat2));
+		productRepository.saveAll(Arrays.asList(p1, p2, p3));
 	}
-
 }
--- src/main/java/com/cmp/springionic/domain/Product.java
+++ src/main/java/com/cmp/springionic/domain/Product.java
@@ -8,6 +8,8 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
 import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
@@ -17,20 +19,24 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Entity
-@Table(name = "tb_category")
+@Table(name = "tb_product")
 @NoArgsConstructor
 @AllArgsConstructor
 @Getter
 @Setter
 @EqualsAndHashCode
-public class Category implements Serializable {
+public class Product implements Serializable {
 	private static final long serialVersionUID = 1L;
-	
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String name;
-	
-	@ManyToMany(mappedBy = "categories")
-	private final List<Product> products = new ArrayList<>();
+	private Double price;
+
+	@ManyToMany
+	@JoinTable(name = "product_category", 
+		joinColumns = @JoinColumn(name = "product_id"), 
+		inverseJoinColumns = @JoinColumn(name = "category_id"))
+	private final List<Category> categories = new ArrayList<>();
 }
