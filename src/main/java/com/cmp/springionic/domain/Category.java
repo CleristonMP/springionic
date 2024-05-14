9ccc5a97eb8896d799fe6ecc8ac19f26f5534cb5
Cleriston Pereira <cleriston.melo.pereira@gmail.com>
2024-05-14 15:30:41 -0300

    H2 database

--- src/main/java/com/cmp/springionic/domain/Category.java
+++ src/main/java/com/cmp/springionic/domain/Category.java
@@ -2,12 +2,19 @@ package com.cmp.springionic.domain;
 
 import java.io.Serializable;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+@Entity
+@Table(name = "tb_category")
 @NoArgsConstructor
 @AllArgsConstructor
 @Getter
@@ -16,6 +23,8 @@ import lombok.Setter;
 public class Category implements Serializable {
 	private static final long serialVersionUID = 1L;
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String name;
 }
