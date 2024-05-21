ac902208a69357b58c1cf57e4139fbca4e01fa17
Cleriston Pereira <cleriston.melo.pereira@gmail.com>
2024-05-21 15:08:50 -0300

    Inserting Category with POST

--- src/main/java/com/cmp/springionic/resources/CategoryResource.java
+++ src/main/java/com/cmp/springionic/resources/CategoryResource.java
@@ -1,19 +1,24 @@
 package com.cmp.springionic.resources;
 
+import java.net.URI;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.cmp.springionic.domain.Category;
 import com.cmp.springionic.services.CategoryService;
 
 @RestController
-@RequestMapping(value="/categories")
+@RequestMapping(value = "/categories")
 public class CategoryResource {
-	
+
 	@Autowired
 	private CategoryService service;
 
@@ -21,5 +26,13 @@ public class CategoryResource {
 	public ResponseEntity<Category> find(@PathVariable Long id) {
 		Category obj = service.search(id);
 		return ResponseEntity.ok().body(obj);
+	}
+
+	@PostMapping
+	public ResponseEntity<Void> insert(@RequestBody Category obj) {
+		obj = service.insert(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
+				.path("/{id}").buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).build();
 	}
 }
