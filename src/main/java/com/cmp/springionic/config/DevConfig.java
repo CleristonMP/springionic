880f96581878ab3ff5599752e19103b68fac71e9
Cleriston Pereira <cleriston.melo.pereira@gmail.com>
2024-05-28 09:50:03 -0300

    Dev profile created

---
+++ src/main/java/com/cmp/springionic/config/DevConfig.java
@@ -0,0 +1,32 @@
+package com.cmp.springionic.config;
+
+import java.text.ParseException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import com.cmp.springionic.services.DBService;
+
+@Configuration
+@Profile("dev")
+public class DevConfig {
+	
+	@Autowired
+	private DBService dbService;
+	
+	@Value("${spring.jpa.hibernate.ddl-auto}")
+	private String strategy;
+
+	@Bean
+	boolean instantiateDatabase() throws ParseException {
+		if (!"create".equals(strategy)) {
+			return false;
+		}
+		
+		dbService.instantiateTestDatabase();
+		return true;
+	}
+}
