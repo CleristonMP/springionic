d5679d14aeca12a4100e4adea2142c67e5e8491e
Cleriston Pereira <cleriston.melo.pereira@gmail.com>
2024-05-29 16:57:27 -0300

    Initial configuration of Spring Security

---
+++ src/main/java/com/cmp/springionic/config/SecurityConfig.java
@@ -0,0 +1,56 @@
+package com.cmp.springionic.config;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+	
+	@Autowired
+    private Environment env;
+
+	private static final String[] PUBLIC_MATCHERS = { "/h2-console/**" };
+
+	private static final String[] PUBLIC_MATCHERS_GET = { "/products/**", "/categories/**" };
+
+	@Bean
+	SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+		
+		if (Arrays.asList(env.getActiveProfiles()).contains("test")) {
+			http.headers((headers) -> headers.frameOptions(withDefaults()).disable());
+        }
+		
+		http.cors(withDefaults()).csrf((csrf) -> csrf.disable());
+		http.authorizeHttpRequests(
+				(authz) -> authz.requestMatchers(PUBLIC_MATCHERS).permitAll()
+				.requestMatchers(HttpMethod.GET, PUBLIC_MATCHERS_GET).permitAll()
+				.anyRequest().authenticated())
+				.httpBasic(withDefaults());
+		http.sessionManagement(
+				(sessionManagement) -> sessionManagement
+					.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+		return http.build();
+	}
+
+	@Bean
+	CorsConfigurationSource corsConfigurationSource() {
+		final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		source.registerCorsConfiguration("/**", new CorsConfiguration().applyPermitDefaultValues());
+		return source;
+	}
+}
